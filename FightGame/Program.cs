fa74a644d132961a01d04c15e190bc3b64652bca
Moise Tshibwabwa <moise.tshibwabwa@learnet.se>
2020-10-15 11:14:26 +0200

    Save: Start

---
+++ FightGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int player1;
+            int player2;
+            int hp;
+            Random number = new Random();
+            int score = number.Next(1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
